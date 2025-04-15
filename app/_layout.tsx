1abc236dca6aff3b31404ac96d9fa39bdff41f7e
OM JADHAV <omjadhav1053@gmail.com>
2025-04-15 16:04:54 +0530

    Initial updates

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
+}
